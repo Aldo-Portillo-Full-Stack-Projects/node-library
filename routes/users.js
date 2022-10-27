28c9b911f63096a409ec19bed92c816b6342bb38
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-27 16:28:36 -0500

    Write another route in users file

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/cool", function(req, res) {
+  res.send('this is the cool page under users.js')
+})
+
 module.exports = router;
