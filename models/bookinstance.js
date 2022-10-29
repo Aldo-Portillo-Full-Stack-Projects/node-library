75068473e032498d1e8d3de3ff4279593dc27267
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-10-29 13:42:48 -0500

    Format due date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema;
@@ -17,6 +18,12 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual("url").get(function () {
 
     return `/catalog/bookinstance/${this._id}`;
+  });
+
+//Add virtual property dueback format
+
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+    return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
   });
   
   // Export model
